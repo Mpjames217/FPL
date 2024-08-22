49fd60cdaaf0a8471ca4b7238daa40608941aa29
Mpjames217 <171149609+Mpjames217@users.noreply.github.com>
2024-08-22 09:39:42 +0100

    Club check added and form delta bug fixed

--- transfer_suggester.py
+++ transfer_suggester.py
@@ -5,17 +5,26 @@ from operator import itemgetter
 
 #Pull down info about team - including available transfers
 max_transfers = 5
-team_info = requests.get('https://fantasy.premierleague.com/api/entry/8035167/event/1/picks').json()
+team_id = '8035167'
+current_GW = '1' #fetch this from API
+address = 'https://fantasy.premierleague.com/api/entry/' + team_id + '/event/' + current_GW + '/picks'
+team_info = requests.get(address).json()
 
-#Get full info on players in squad
+#Get full info on players in squad and count players per club
 r = requests.get('https://fantasy.premierleague.com/api/bootstrap-static').json()
 all_players = r['elements']
 squad_players = [] # could compile seperate list of starters and bench players
+
+#initiate club counter dictionary
+clubs = {}
+for i in range(1,21):
+    clubs[i] = 0
 
 for player in all_players:
     for squad_player in team_info['picks']:
         if player['id'] == squad_player['element']:
             squad_players.append(player)
+            clubs[player['team']] += 1
 
 
 #get top players by form for each position - exluding players already in team and players with injuries
@@ -30,8 +39,10 @@ possible_transfers = []
 
 for form_player in form_players:
     for squad_player in squad_players:
-        if form_player['element_type'] == squad_player['element_type'] and form_player['form'] > squad_player['form'] and form_player['now_cost'] <= squad_player['now_cost']:
-            possible_transfers.append({'player_in': form_player['web_name'], 'player_out': squad_player['web_name'], 'form_delta': float(form_player['form']) - float(squad_player['form'])})
+        if form_player['element_type'] == squad_player['element_type'] and float(form_player['form']) > float(squad_player['form']) + 2 and form_player['now_cost'] <= squad_player['now_cost']:
+            #check max players per club not exceeded
+            if clubs[form_player['team']] < 3 or form_player['team'] == squad_player['team']:
+                possible_transfers.append({'player_in': form_player['web_name'], 'player_out': squad_player['web_name'], 'form_delta': float(form_player['form']) - float(squad_player['form'])})
 
 #order transfers by point_delta
 possible_transfers = sorted(possible_transfers, key=itemgetter('form_delta'), reverse=True)
