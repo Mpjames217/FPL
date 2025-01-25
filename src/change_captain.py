2ca43a3f55e57198db70f9acec8d59486a5db6e9
Mpjames217 <171149609+Mpjames217@users.noreply.github.com>
2025-01-25 11:31:59 +0000

    change_captain.py added - draft script to change captain through api

---
+++ src/change_captain.py
@@ -0,0 +1,55 @@
+import requests, os, dotenv
+from pprint import pprint
+
+dotenv.load_dotenv()
+
+pl_profile = os.environ.get('FPL_COOKIE')
+datadtome = os.environ.get('FPL_COOKIE2')
+team_id = os.environ.get('team_id')
+email = os.environ.get('FPL_EMAIL')
+password = os.environ.get('FPL_PASSWORD')
+
+s = requests.Session()
+
+payload = {
+    "login": email,
+    "password": password,
+    "app": "plfpl-web",
+    "redirect_uri": "https://fantasy.premierleague.com/a/login"
+}
+
+headers = {
+    "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 10; PRO 5 Build/LMY47D)",
+    "Cookie": f"datadome={datadtome};pl_profile={pl_profile}"
+}
+
+login_url = "https://users.premierleague.com/accounts/login/"
+response = s.post(login_url, data=payload,headers=headers)
+
+print(response)
+
+url = f"https://fantasy.premierleague.com/api/my-team/{os.environ.get('team_id')}/"
+
+response = s.get(url)
+
+print(response)
+
+team = response.json()
+
+new_captain_id = 401
+new_vice_captain_id = 328
+
+# Update the team lineup with the new captain and vice-captain
+for player in team['picks']:
+    if player["element"] == new_captain_id:
+        player["is_captain"] = True
+    else:
+        player["is_captain"] = False
+
+    if player["element"] == new_vice_captain_id:
+        player["is_vice_captain"] = True
+    else:
+        player["is_vice_captain"] = False
+
+response = s.post(url, json=team)
+print(response)
